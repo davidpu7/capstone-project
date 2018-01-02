d177ba6d35cceac52364342d99931cbd539542e6
davidpu <davidpu7@gmail.com>
2018-01-02 00:31:38 +0000

    fix app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-f <li><a target="_blank" href="https://www.heroku.com/">Heroku</a></li>
-            <li><a target="_blank" href="http://flask.pocoo.org/">Flask</a></li>
-            <li><a target="_blank" href="http://jinja.pocoo.org/docs/2.9/">Jinja2</a></li>
-            <tstrap</a></li>rom flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
