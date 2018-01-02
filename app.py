ac1f33609b08c0531c70dbed53c5f5bc2df45736
davidpu <davidpu7@gmail.com>
2018-01-02 06:19:34 +0000

    modify close open value

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import requests as rq
+import quandl as qd
 
 app = Flask(__name__)
 
@@ -7,14 +8,23 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
-@app.route('/view_ticker', method=['GET', 'POST'])
+@app.route('/view_ticker', methods=['GET', 'POST'])
 def view_ticker():
-    stock = rq.form['ticker']
-    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock 
-    session = rq.Session()
-    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-    raw_data = session.get(api_url)
-    return raw
+    stock = request.form['ticker']
+    value = 0.4
+
+    if 'close' in request.form:
+        value = 0.4
+    if 'adj_close' in request.form:
+        value = 0.10
+    if 'open' in request.form:
+        value = 0.1
+    if 'adj_open' in request.form:
+        value = 0.7
+
+    mydata =  qd.get("WIKI/" + stock + value, rows = 20, api_key='oSvidbxNa84mVv7Kzqh2')
+    html = file_html(mydata)
+    return html
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
