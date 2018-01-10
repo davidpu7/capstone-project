99aa2d04163858d1115645c4ab1295bb031f0fae
davidpu <davidpu7@gmail.com>
2018-01-10 21:00:43 +0000

    change

--- app.py
+++ app.py
@@ -13,31 +13,30 @@ def index():
 
 @app.route('/view_ticker', methods=['GET', 'POST'])
 def view_ticker():
-    if request.method == 'POST':
-        stock = request.form['ticker']
+    stock = request.form['ticker']
+    value = '.4'
+    status = 'Close'
+    if request.form.get('box1'):
         value = '.4'
         status = 'Close'
-        if request.form.get('box1'):
-            value = '.4'
-            status = 'Close'
-        if request.form.get('box2'):
-            value = '.11'
-            status = 'Adj. Close'
-        if request.form.get('box3'):
-            value = '.1'
-            status = 'Open'
-        if request.form.get('box4'):
-            value = '.8'
-            status = 'Adj. Open'
+    if request.form.get('box2'):
+        value = '.11'
+        status = 'Adj. Close'
+    if request.form.get('box3'):
+        value = '.1'
+        status = 'Open'
+    if request.form.get('box4'):
+        value = '.8'
+        status = 'Adj. Open'
 
-        mydata = qd.get("WIKI/" + stock + value, rows = 20, api_key='oSvidbxNa84mVv7Kzqh2')
-        p = figure(x_axis_type = 'datetime', title = status + " Price for " + request.form['ticker'])
-        p.line('Date', status, source=mydata)
-        p.xaxis.axis_label = "Date"
-        p.yaxis.axis_label = "Price"
-        html = file_html(p, CDN, "my plot")
+    mydata = qd.get("WIKI/" + stock + value, rows = 20, api_key='oSvidbxNa84mVv7Kzqh2')
+    p = figure(x_axis_type = 'datetime', title = status + " Price for " + request.form['ticker'])
+    p.line('Date', status, source=mydata)
+    p.xaxis.axis_label = "Date"
+    p.yaxis.axis_label = "Price"
+    html = file_html(p, CDN, "my plot")
 
-        return html
+    return html
 
 if __name__ == '__main__':
   app.run(port=33507)
